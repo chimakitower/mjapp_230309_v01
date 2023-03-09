2993b2792dfb328c0854ff651084183d83b26f07
chimaki_tower <chimaki_tower@tower.local>
2023-03-09 12:44:52 +0900

    1st commmit

--- src/app.py
+++ src/app.py
@@ -10,9 +10,8 @@ from function import *
 #　xは1行13列のndarrayでIDとしたい  
 def output_predict(x): 
     # 学習済みモデル（iris.pkl）を読み込み
+    # model = joblib.load('./src/mjapp.pkl')
     model = joblib.load('mjapp.pkl')
-    # x = get_onehot(x)
-    # x = flatten(x)
     pred_label = model.predict(x.reshape(1, -1))
     return pred_label
 
@@ -31,124 +30,15 @@ app = Flask(__name__)
 
 # 入力フォームの設定
 class TehaiForm(Form):
-    tehai1 = StringField('6巡目の手牌:1枚目',
-                    [validators.InputRequired(message='記入形式が正しくありません')])
-    tehai2 = StringField('6巡目の手牌:2枚目',
-                    [validators.InputRequired(message='記入形式が正しくありません')])
-    tehai3 = StringField('6巡目の手牌:3枚目',
-                    [validators.InputRequired(message='記入形式が正しくありません')])
-    tehai4 = StringField('6巡目の手牌:4枚目',
-                    [validators.InputRequired(message='記入形式が正しくありません')])
-    tehai5 = StringField('6巡目の手牌:5枚目',
-                    [validators.InputRequired(message='記入形式が正しくありません')])
-    tehai6 = StringField('6巡目の手牌:6枚目',
-                    [validators.InputRequired(message='記入形式が正しくありません')])
-    tehai7 = StringField('6巡目の手牌:7枚目',
-                    [validators.InputRequired(message='記入形式が正しくありません')])
-    tehai8 = StringField('6巡目の手牌:8枚目',
-                    [validators.InputRequired(message='記入形式が正しくありません')])
-    tehai9 = StringField('6巡目の手牌:9枚目',
-                    [validators.InputRequired(message='記入形式が正しくありません')])
-    tehai10 = StringField('6巡目の手牌:10枚目',
-                    [validators.InputRequired(message='記入形式が正しくありません')])
-    tehai11 = StringField('6巡目の手牌:11枚目',
-                    [validators.InputRequired(message='記入形式が正しくありません')])
-    tehai12 = StringField('6巡目の手牌:12枚目',
-                    [validators.InputRequired(message='記入形式が正しくありません')])
-    tehai13 = StringField('6巡目の手牌:13枚目',
-                    [validators.InputRequired(message='記入形式が正しくありません')])
-
-    def validate_tehai1(self, tehai1):
-        if tehai1.data == "":
+    tehai = StringField('6巡目の手牌を記入例に従って記入してください',
+                    [validators.InputRequired()])
+    
+    def validate_tehai(self, tehai):
+        if tehai.data == "":
             raise ValidationError("牌を入力してください")
 
-        elif tehai1.data not in sanma_pai_to_num:
-            raise ValidationError("例のように記入してください。（あるいは、マンズの2~8を記入していませんか？）")
-
-    def validate_tehai2(self, tehai2):
-        if tehai2.data == "":
-            raise ValidationError("牌を入力してください")
-
-        elif tehai2.data not in sanma_pai_to_num:
-            raise ValidationError("例のように記入してください。（あるいは、マンズの2~8を記入していませんか？）")
-
-    def validate_tehai3(self, tehai3):
-        if tehai3.data == "":
-            raise ValidationError("牌を入力してください")
-
-        elif tehai3.data not in sanma_pai_to_num:
-            raise ValidationError("例のように記入してください。（あるいは、マンズの2~8を記入していませんか？）")
-
-    def validate_tehai4(self, tehai4):
-        if tehai4.data == "":
-            raise ValidationError("牌を入力してください")
-
-        elif tehai4.data not in sanma_pai_to_num:
-            raise ValidationError("例のように記入してください。（あるいは、マンズの2~8を記入していませんか？）")
-
-    def validate_tehai5(self, tehai5):
-        if tehai5.data == "":
-            raise ValidationError("牌を入力してください")
-
-        elif tehai5.data not in sanma_pai_to_num:
-            raise ValidationError("例のように記入してください。（あるいは、マンズの2~8を記入していませんか？）")
-
-    def validate_tehai6(self, tehai6):
-        if tehai6.data == "":
-            raise ValidationError("牌を入力してください")
-
-        elif tehai6.data not in sanma_pai_to_num:
-            raise ValidationError("例のように記入してください。（あるいは、マンズの2~8を記入していませんか？）")
-
-    def validate_tehai7(self, tehai7):
-        if tehai7.data == "":
-            raise ValidationError("牌を入力してください")
-
-        elif tehai7.data not in sanma_pai_to_num:
-            raise ValidationError("例のように記入してください。（あるいは、マンズの2~8を記入していませんか？）")
-
-    def validate_tehai8(self, tehai8):
-        if tehai8.data == "":
-            raise ValidationError("牌を入力してください")
-
-        elif tehai8.data not in sanma_pai_to_num:
-            raise ValidationError("例のように記入してください。（あるいは、マンズの2~8を記入していませんか？）")
-
-    def validate_tehai9(self, tehai9):
-        if tehai9.data == "":
-            raise ValidationError("牌を入力してください")
-
-        elif tehai9.data not in sanma_pai_to_num:
-            raise ValidationError("例のように記入してください。（あるいは、マンズの2~8を記入していませんか？）")
-
-    def validate_tehai10(self, tehai10):
-        if tehai10.data == "":
-            raise ValidationError("牌を入力してください")
-
-        elif tehai10.data not in sanma_pai_to_num:
-            raise ValidationError("例のように記入してください。（あるいは、マンズの2~8を記入していませんか？）")
-
-    def validate_tehai11(self, tehai11):
-        if tehai11.data == "":
-            raise ValidationError("牌を入力してください")
-
-        elif tehai11.data not in sanma_pai_to_num:
-            raise ValidationError("例のように記入してください。（あるいは、マンズの2~8を記入していませんか？）")
-
-    def validate_tehai12(self, tehai12):
-        if tehai12.data == "":
-            raise ValidationError("牌を入力してください")
-
-        elif tehai12.data not in sanma_pai_to_num:
-            raise ValidationError("例のように記入してください。（あるいは、マンズの2~8を記入していませんか？）")
-
-    def validate_tehai13(self, tehai13):
-        if tehai13.data == "":
-            raise ValidationError("牌を入力してください")
-
-        elif tehai13.data not in sanma_pai_to_num:
-            raise ValidationError("例のように記入してください。（あるいは、マンズの2~8を記入していませんか？）")
-
+        # elif tehai.data not in sanma_pai_to_num:
+        #     raise ValidationError("例のように記入してください。（あるいは、マンズの2~8を記入していませんか？）")
 
     # html 側で表示する submit ボタンの設定
     submit = SubmitField('送信')
@@ -167,22 +57,11 @@ def predicts():
             return render_template('index.html', forms=tehaiForm)
         # 条件に当てはまる場合の、推論を実行
         else:
-            pai1 = sanma_pai_to_num[str(request.form['tehai1'])]
-            pai2 = sanma_pai_to_num[str(request.form['tehai2'])]
-            pai3 = sanma_pai_to_num[str(request.form['tehai3'])]
-            pai4 = sanma_pai_to_num[str(request.form['tehai4'])]
-            pai5 = sanma_pai_to_num[str(request.form['tehai5'])]
-            pai6 = sanma_pai_to_num[str(request.form['tehai6'])]
-            pai7 = sanma_pai_to_num[str(request.form['tehai7'])]
-            pai8 = sanma_pai_to_num[str(request.form['tehai8'])]
-            pai9 = sanma_pai_to_num[str(request.form['tehai9'])]
-            pai10 = sanma_pai_to_num[str(request.form['tehai10'])]
-            pai11 = sanma_pai_to_num[str(request.form['tehai11'])]
-            pai12 = sanma_pai_to_num[str(request.form['tehai12'])]
-            pai13 = sanma_pai_to_num[str(request.form['tehai13'])]
-
+            pai = str(request.form['tehai'])
+            pai_list = split_pai(pai)
+            x = np.array(pai_list)
+            
             # 入力された値を np.array に変換して推論
-            x = np.array([pai1, pai2, pai3, pai4, pai5, pai6, pai7, pai8, pai9, pai10, pai11, pai12, pai13])
             x = get_onehot(x)
             x = flatten(x)
             pred = output_predict(x)
@@ -191,11 +70,6 @@ def predicts():
 
     # GET 　メソッドの定義
     elif request.method == 'GET':
-        # import os
-        # path = os.getcwd()
-        # print(path) 
- 
-
         return render_template('index.html', forms=tehaiForm)
 
 # アプリケーションの実行
--- src/function.py
+++ src/function.py
@@ -19,3 +19,37 @@ sanma_pai_to_num = {'1m': 0, '9m': 1,
 '1p': 2, '2p': 3, '3p': 4, '4p': 5, '5p': 6, '6p': 7, '7p': 8, '8p': 9, '9p': 10, 
 '1s': 11, '2s': 12, '3s': 13, '4s': 14, '5s': 15, '6s': 16, '7s': 17, '8s': 18, '9s': 19, 
 '東': 20, '南': 21, '西': 22, '北': 23, '白': 24, '撥': 25, '中': 26}
+
+jihai_dict = {'1':'東', '2':'南', '3':'西', '4':'北', '5':'白', '6':'撥', '7':'中'}
+
+def split_pai(pai):
+
+    dummy = [char for char in pai]
+    m_list, p_list, s_list, z_list = [], [], [], []
+    pre = []
+    for char in dummy:
+        if (char != 'm') and (char != 'p') and (char != 's') and (char != 'z'):
+            pre.append(char)
+        
+        elif char == 'm':
+            m_pre = [sanma_pai_to_num[char + 'm'] for char in pre]
+            m_list += m_pre
+            pre = []
+        
+        if char == 'p':
+            p_pre = [sanma_pai_to_num[char + 'p'] for char in pre]
+            p_list += p_pre
+            pre = []
+
+        elif char == 's':
+            s_pre = [sanma_pai_to_num[char + 's'] for char in pre]
+            s_list += s_pre
+            pre = []
+            
+        elif char == 'z':
+            z_pre = [sanma_pai_to_num[jihai_dict[char]] for char in pre]
+            z_list += z_pre
+            pre = []
+        
+        pai_list = m_list + p_list + s_list + z_list
+    return pai_list
